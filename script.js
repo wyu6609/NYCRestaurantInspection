ee23b03b2e64cb9f96bbb5f74fbe624b273c750f
William Yu <88666309+wyu6609@users.noreply.github.com>
2021-12-27 15:27:34 -0500

    added search function

--- script.js
+++ script.js
@@ -17,9 +17,42 @@ let app = {
   displayAPI: function (data) {
     // take json and store to js variables
     console.log(data);
-    for (i = 0; i < 5; i++) {
-      console.log(data[i].phone);
+    const sliceData = data.slice(0, 3);
+    console.log(sliceData);
+
+    function create_h3(content) {
+      let h3 = document.createElement("h3");
+      h3.textContent = element.dba;
+      return h3;
     }
+
+    let Arr = [];
+    let grade = document.querySelector(".grade");
+    let h3 = document.createElement;
+    sliceData.forEach((element) => {
+      let grade = document.querySelector(".user-display");
+      let content = `
+        <div class = "sects">
+          <p class="address">zipcode: ${element.dba} ${element.zipcode}</p>
+        
+          <p class="grade">Grade: ${element.grade}</p>
+        </div>`;
+
+      grade.insertAdjacentHTML("afterend", content);
+    });
+
+    // console.log(
+    //   element.dba +
+    //     "\n" +
+    //     element.building +
+    //     " " +
+    //     element.street +
+    //     " " +
+    //     element.zipcode +
+    //     "\n" +
+    //     "GRADE: " +
+    //     element.grade
+    // )
   },
   // data to frontend (query selectors)
 
